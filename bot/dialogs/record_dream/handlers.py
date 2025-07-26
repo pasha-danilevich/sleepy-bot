18c7502b7f950f8c0da4dbba3acf4042beaec9dc
pasha_danilevich <danilevitch.pasha@yandex.ru>
2025-07-26 17:10:30 +0300

    Рефакторинг логирования и конфигурации pre-commit
    - Упрощена конфигурация mypy в pre-commit, удалены закомментированные аргументы
    - Добавлено логирование в handlers.py вместо print
    - Рефакторинг CallbackLoggerMiddleware:
      - Добавлены константы и улучшена структура кода
      - Улучшено форматирование логов с использованием click
      - Разделена логика на методы
      - Удален лишний raise ValueError
    - Добавлен новый логгер bot_event_log в конфигурацию

--- bot/dialogs/record_dream/handlers.py
+++ bot/dialogs/record_dream/handlers.py
@@ -1,11 +1,15 @@
+import logging
 from datetime import date
 from typing import TypedDict
 
 from aiogram_dialog import DialogManager
+
+logger = logging.getLogger(__name__)
+
 
 class StartData(TypedDict):
     create_date: date
 
 
 async def on_start(start_data: StartData, manager: DialogManager) -> None:
-    print(start_data)
+    logger.debug(f'{start_data=}')
--- bot/middlewares/callback_logger.py
+++ bot/middlewares/callback_logger.py
@@ -1,14 +1,17 @@
 import logging
 from typing import Any, Awaitable, Callable, Dict, cast
 
+import click
 from aiogram import BaseMiddleware
 from aiogram.types import CallbackQuery, Message, TelegramObject, Update
 from aiogram_dialog.api.entities import Context
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('bot_event_log')
 
 
 class CallbackLoggerMiddleware(BaseMiddleware):
+    EMPTY_STRING = ''
+
     async def __call__(
         self,
         handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
@@ -20,18 +23,31 @@ class CallbackLoggerMiddleware(BaseMiddleware):
 
         callback = event_update.callback_query
         if callback is None:
+            logger.warning("Логгирование не производиться: нет callback_query")
             return result
 
         context: Context = data['aiogd_context']
-
-        data_text = f' | data: {context.dialog_data} ' if context.dialog_data else ''
-        start_data_text = f' | start_data: {context.start_data} ' if context.start_data else ''
-
-        logger.debug(
-            f'Click on: "{self.get_button_text(callback)}"{data_text}{start_data_text}'
-        )
+        self._log(context, callback)
 
         return result
+
+    def _log(self, context: Context, callback: CallbackQuery) -> None:
+        data_text = (
+            f' dialog_data: {context.dialog_data}'
+            if context.dialog_data
+            else self.EMPTY_STRING
+        )
+        start_data_text = (
+            f' | start_data: {context.start_data} '
+            if context.start_data
+            else self.EMPTY_STRING
+        )
+        msg = click.style('Click on:', fg='cyan')
+        logger.debug(msg + ' ' + self.get_button_text(callback))
+
+        if data_text or start_data_text:
+            msg = click.style('└──> With "Context":', fg='cyan')
+            logger.debug(msg + ' ' + data_text + start_data_text)
 
     @staticmethod
     def get_button_text(callback: CallbackQuery) -> str:
@@ -52,7 +68,6 @@ class CallbackLoggerMiddleware(BaseMiddleware):
                     if button.callback_data == pressed_data:
                         text = button.text
                         return text
-                    raise ValueError('Неизвестный callback')
 
         raise ValueError('Нет кнопок')
 
